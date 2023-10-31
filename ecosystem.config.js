83612feb67f6cc4a81f3094ec72f30323b0c2f7a
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-10-31 20:55:00 +0100

    :rocket: Configure deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,4 +15,16 @@ module.exports = {
       NODE_ENV: 'development',
     },
   }],
+  deploy : {
+    production : {
+      user : 'sikessem',
+      host : '102.219.179.57',
+      ref  : 'origin/main',
+      repo : 'https://github.com/sikessem/sikessem.com.git',
+      path : '/apps/sikessem.com',
+      'pre-deploy-local': '',
+      'post-deploy' : 'bun install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
 };
