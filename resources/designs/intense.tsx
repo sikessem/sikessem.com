a7daa8d48e959aadb03ae2899853f35bc05521ba
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-01-18 15:39:01 +0100

    :sparkles: Replace Qwik directives with Qwik components

--- resources/designs/intense.tsx
+++ resources/designs/intense.tsx
@@ -1,4 +1,4 @@
-import { type QwikJSX, render, component$ } from "@builder.io/qwik";
+import { type QwikJSX, render, component$, Component } from "@builder.io/qwik";
 import { parse_props, parse_val } from './parser';
 
 export type Props = Record<any, any>;
@@ -14,45 +14,74 @@ export declare const Renderer : {
     new(): Renderer;
 };
 
-class Element extends HTMLElement {}
+class QElement extends HTMLElement {}
 
 export function use(rendererType: typeof Renderer, prefix = 'q') {
     const renderer = new rendererType;
     const template = renderer.render;
     const Component = component$(<T extends Props>(props: T) => template(props));
+
     const tag = `${prefix}-${renderer.name}`;
     if (! customElements.get(tag)) {
-        customElements.define(tag, Element);
+        customElements.define(tag, QElement);
     }
-    for (const elt of document.getElementsByTagName(tag)) {
-        const props: Props = {};
-        const slot = elt.innerHTML;
-        const reg = /render\s*\(\{([^\(\)\{\}]+?)\}\)\s*(?:\=\>)?\s*\{?/mg
-        const tpl = template.toString();
-        const res = reg.exec(tpl);
-        if (res) {
-            const default_props = parse_props(res[1]);
-            for (const [prop, default_val] of Object.entries(default_props)) {
-                let attr = elt.getAttribute(`:${prop}`);
-                if (attr) {
-                    props[prop] = parse_val(attr);
-                    continue;
-                }
 
-                attr = elt.getAttribute(prop);
-                if (attr) {
-                    props[prop] = attr;
-                    continue;
-                }
+    const reg = /render\s*\(\{([^\(\)\{\}]+?)\}\)\s*(?:\=\>)?\s*\{?/mg
+    const tpl = template.toString();
+    const res = reg.exec(tpl);
+    const props = res ? parse_props(res[1]): {};
 
-                props[prop] = default_val;
+    for (const elt of document.querySelectorAll(`[${tag}]`)) {
+        elt.removeAttribute(tag);
+
+        const attrs = elt.attributes;
+        const q_elt = document.createElement(tag);
+        for (const attr of Array.from(attrs)) {
+            q_elt.setAttribute(attr.name, attr.value);
+        };
+
+        if (elt.childNodes.length > 0) {
+            for (const child of Array.from(elt.childNodes)) {
+                q_elt.appendChild(child.cloneNode(true));
             };
+        } else {
+            q_elt.textContent = elt.textContent;
         }
-        render(
-            elt,
-            <Component {...props}>
-                {slot}
-            </Component>,
-        );
+
+        elt.parentNode
+        ? elt.parentNode.replaceChild(q_elt, elt)
+        : elt.replaceWith(q_elt);
     }
+
+    for (const elt of document.getElementsByTagName(tag)) {
+        make(elt, props, Component);
+    }
+}
+
+export function make(elt: Element, props: Props, Comp: Component) {
+    const _props: Props = {};
+    const slot = elt.innerHTML;
+    if (props) {
+        for (const [_prop, _val] of Object.entries(props)) {
+            let attr = elt.getAttribute(`:${_prop}`);
+            if (attr) {
+                _props[_prop] = parse_val(attr);
+                continue;
+            }
+
+            attr = elt.getAttribute(_prop);
+            if (attr) {
+                _props[_prop] = attr;
+                continue;
+            }
+
+            _props[_prop] = _val;
+        };
+    }
+    render(
+        elt,
+        <Comp {..._props}>
+            {slot}
+        </Comp>,
+    );
 }
