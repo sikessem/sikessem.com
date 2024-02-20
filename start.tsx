963a4de35786e28a4f46963ee81cdc9756b8a5ce
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-02-20 15:11:41 +0100

    :zap: Use Deno to serve public assets instead of Qwik

--- start.tsx
+++ start.tsx
@@ -1,9 +1,36 @@
+import path from "node:path";
 import serveFontend from "./design/server/entry.deno.js";
+import { get_extension_type } from './mime.ts';
 
-Deno.serve(async (request: Request) => {
+Deno.serve(async (request: Request): Promise<Response> => {
   const response = await (serveFontend(request) as Promise<Response>);
   if (response.ok) {
     return response;
   }
+
+  const url = new URL(request.url);
+  const pathname = decodeURIComponent(url.pathname);
+
+  try {
+    const asset = Deno.realPathSync(`${Deno.cwd()}/assets/${pathname}`);
+    if (Deno.statSync(asset).isFile) {
+      const file = Deno.readFileSync(asset);
+      const ext = path.extname(asset);
+      console.log(`Serve asset ${asset}`);
+      return new Response(file, {
+        headers: {
+          'Content-Type': get_extension_type(ext),
+        },
+      });
+    }
+  } catch(error) {
+    if (
+      error !instanceof Deno.errors.NotFound &&
+      error !instanceof Deno.errors.IsADirectory
+    ) {
+      throw error;
+    }
+  }
+
   return new Response("Welcome to Sikessem");
 });
