5d08f5c7318ca8516633c267f897a0fcf84cb845
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-03-26 20:16:25 +0100

    :zap: Serve files using Phun

--- start.ts
+++ start.ts
@@ -1,14 +1,29 @@
-import { use } from "phun";
+import { existsSync } from "node:fs";
+import { join } from "node:path";
+import { serve } from "phun";
+import type { Handler } from "sikessem/base/routing";
+import { FileSystem } from "sikessem/core/fs";
+import { ActionRouter, FileRouter } from "sikessem/core/routing";
 
 const server = Bun.serve({
-  async fetch(_req: Request): Promise<Response> {
-    const content = await use(`${import.meta.dir}/website/public/index.php`);
-    return new Response(content, {
-      status: 200,
-      headers: {
-        "Content-Type": "text/html; charset=utf-8",
-      },
-    });
+  async fetch(request: Request, server): Promise<Response> {
+    const handlers: Handler[] = [];
+    const assets_path = join(import.meta.dir, "/website/public");
+
+    if (existsSync(assets_path)) {
+      const assets_fs = new FileSystem(assets_path);
+      const assets_router = new FileRouter(assets_fs);
+      const response = assets_router.handle(request);
+      if (response.ok) {
+        return response;
+      }
+    }
+
+    return await serve(
+      `${import.meta.dir}/website/public/index.php`,
+      request,
+      server,
+    );
   },
 });
 
